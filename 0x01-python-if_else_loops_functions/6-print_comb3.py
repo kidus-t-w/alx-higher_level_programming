107b09d7ee8ded3c165accb5c94b6f06225c3147
kidus-t-w <kidusk69@gmail.com>
2023-01-15 21:26:53 +0300

    added 6-print_comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0, 8):
-    for x in range(0, 9):
-        if (i < x):
-            print("{:d}{:d}".format(i, x), end=", ")
-print("{:d}{:d}".format(8, 9))
+for x in range(0, 8):
+    for y in range(0, 10):
+        if (x < y):
+            print("{}{}, ".format(x, y), end='')
+print("{}{}".format(8, 9))
