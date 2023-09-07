e65c9a504c8ef65cb907e47216a9fd7f3c17370f
kidus-t-w <kidusk69@gmail.com>
2023-09-07 19:14:41 +0300

    Updated 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argvalues = sys.argv
+    for i in range(1, len(argvalues)):
+        if len(argvalues) == 2:
+            print("1 argument:\n1: {}".format(argvalues[1]))
+        elif len(argvalues) == 1:
+            print(".")
+        elif len(argvalues) > 2:
+            if i == 1:
+                print("{} arguments".format(len(argvalues) - 1))
+                print("{}: {}".format(i, argvalues[i]))
+            else:
+                print("{}: {}".format(i, argvalues[i]))
