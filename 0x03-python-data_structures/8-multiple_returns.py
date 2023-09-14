627468b9391f3d437548d56e7915f7fa114bb479
kidus-t-w <kidusk69@gmail.com>
2023-09-14 13:23:38 +0300

    updated 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        return(0, None)
+        return(0, "None")
+    
     first_char = sentence[0]
 
     return (len(sentence), first_char)
