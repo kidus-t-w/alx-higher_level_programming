4d03d47c114836b4b89446bdd161d6d33970c437
kidus-t-w <kidusk69@gmail.com>
2023-09-15 18:47:32 +0300

    Added 0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    new = []
+    for row in matrix:
+        new_matrix = [x**2 for x in row]
+        new.append(new_matrix)
+    return new
