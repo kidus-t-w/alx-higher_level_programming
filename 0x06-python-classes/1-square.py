a1ed5b44efd231bdfdd32026da51d3a03e1f5d68
kidus-t-w <kidusk69@gmail.com>
2023-10-06 14:27:00 +0300

    Updated 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """Defininf a square"""
 
+
 class Square:
-    """Initlizes a squre"""
+    """
+    Initlizes a squre
+
+    Args:
+        size (int): Number to be squred
+    """
     def __init__(self, size):
         self.size = size
