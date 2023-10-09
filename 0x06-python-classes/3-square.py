f514bd4a22928708a72d3adc037833183e317cbe
kidus-t-w <kidusk69@gmail.com>
2023-10-09 10:52:15 +0300

    Updated

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Defines a squre"""
 
-class Squre:
-    """Defines a square"""
-    def __init__(self, size):
-        """Initilizing a new squre
+
+class Square:
+    """This class defines a squre"""
+    def __init__(self, size=0):
+        """Initialize a new squre.
 
         Args:
-            size (int): number to be squared
+            size (int): The sie of the new square
         """
-        if isinstance(size, int):
-            raise TypeError("size must be an integer")
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
