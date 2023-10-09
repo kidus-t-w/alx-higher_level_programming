d1496f5bcbebd10f63a758ef3663ca72654f06be
kidus-t-w <kidusk69@gmail.com>
2023-10-09 11:29:48 +0300

    Updated 4-squre.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines a squre"""
+
+
+class Square:
+    """This class defines a squre"""
+    def __init__(self, size=0):
+        """Initialize a new squre.
+
+        Args:
+            size (int): The sie of the new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """calculates the area of a squre"""
+        return self.__size ** 2
+
+    def size(self):
+        """Returns the size value"""
+        return self.__size
+
+    def size(self, value):
+        """Sets size to a give value
+
+        Args:
+            value (int): value which is going to set size
+        """
+        self.__size = value
