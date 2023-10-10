ba4c60b4af90a27aa1bf374206de0984d4ffc946
kidus-t-w <kidusk69@gmail.com>
2023-10-10 22:07:38 +0300

    Added Tasks

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module: 4-inherits_from.py
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if object is an instance of a class 
+    Else returns False
+    Args:
+    obj (object): object to be checked
+    a_class (class): class
+    """
+    result = issubclass(a_class, obj)
+    return result
