197d067b03e01aeffa6a7d08194f6d434c44e480
kidus-t-w <kidusk69@gmail.com>
2023-10-10 10:49:05 +0300

    Added 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Module: 1-my_list"""
+''' Module: 1-my_list
+'''
 
 
 class MyList(list):
+    ''' Represents a MyList
+    '''
 
     def print_sorted(self):
-        """
-        Prints the list, int a sorted format
-        """
+        '''
+        prints the list, but sorted
+        '''
         print(sorted(self))
