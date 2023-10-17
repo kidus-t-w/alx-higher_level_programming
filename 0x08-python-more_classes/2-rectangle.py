2d7569fbfbc5452295d884def45fd2bbdd68d016
kidus-t-w <kidusk69@gmail.com>
2023-10-17 11:30:44 +0300

    Updated 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,19 +6,38 @@ Module: 2-rectangle.py
 
 class Rectangle:
     """
-    Defines a rectangle
+    Defines a rectangle.
     """
     def __init__(self, width=0, height=0):
-        if not isinstance(width, int):
-            raise TypeError("width must be an intiger")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = width
+        """
+        Defines height and width of the rectangle
+        Args:
+            width (int): Width of the rectangle
+            height (int): Height of the rectangle
+        """
         if not isinstance(height, int):
-            raise TypeError("width must be an intiger")
+            raise TypeError("height must be an integer")
         elif height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
     @property
     def width(self):
@@ -27,22 +46,11 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an intiger")
+            raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
-
-    @width.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an intiger")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__width = value
 
     def area(self):
         return (self.__width * self.__height)
