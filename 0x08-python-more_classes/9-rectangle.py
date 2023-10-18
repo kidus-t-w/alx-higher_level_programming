9d8536387ecb63b6f814549813e74a59cd6d7ad1
kidus-t-w <kidusk69@gmail.com>
2023-10-18 12:09:00 +0300

    Updated 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -88,6 +88,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
@@ -97,5 +98,7 @@ class Rectangle:
             return rect_2
         else:
             return rect_1
+
+    @classmethod
     def square(cls, size=0):
         return Rectangle(size, size)
