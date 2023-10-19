0f8c74d73a00ed5f3eb1ce3df56923d1b464ed06
kidus-t-w <kidusk69@gmail.com>
2023-10-19 11:34:05 +0300

    Updated 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,11 @@ Moduel: 0-add_integer.py
 
 
 def add_integer(a, b=98):
+    """Returns the sum of two numbers
+        Args:
+        a (int): first number
+        b (int): second number    
+    """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
