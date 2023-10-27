ec09823bd5c3701d408a6d7c38ff02bd67689b7a
kidus-t-w <kidusk69@gmail.com>
2023-10-27 15:09:47 +0300

    updated validation for setters and  instations

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,12 +16,12 @@ class Rectangle(Base):
         """
         if type(width) != int:
             raise TypeError("width must be an integer")
-        elif width < 0:
+        elif width <= 0:
             raise ValueError("width must be > 0")
         self.__width = width
         if type(height) != int:
             raise TypeError("height must be an integer")
-        elif height < 0:
+        elif height <= 0:
             raise ValueError("height must be > 0")
         self.__height = height
         if type(x) != int:
@@ -44,7 +44,7 @@ class Rectangle(Base):
     def width(self, value):
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -56,7 +56,7 @@ class Rectangle(Base):
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
