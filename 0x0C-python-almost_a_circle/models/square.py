ed79f2ea58dc9bb77f2b3dcf25e395d2a9246c57
kidus-t-w <kidusk69@gmail.com>
2023-10-31 09:16:44 +0300

    Added update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -41,3 +41,16 @@ class Square(Rectangle):
                 if a == 3:
                     self.y == arg
                 a += 1
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
