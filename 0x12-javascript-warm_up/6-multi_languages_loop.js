a6f0fd6d7d9910c0d496d1bda863da3da82fb463
kidus-t-w <kidusk69@gmail.com>
2023-12-13 10:15:59 +0300

    Updated Tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0, len = myArray.length; i < len; i++) {
-  console.log(myArray[i]);
+let stringArray = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < stringArray.length; i++) {
+  console.log(stringArray[i]);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-
-let num = process.argv[2];
-
-if (!isNaN(parseInt(process.argv[2]))) {
-  for (let i = 0; i < num; i++) {
+if (isNaN(parseInt(process.argv[2])) || process.argv[2] === null){
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
