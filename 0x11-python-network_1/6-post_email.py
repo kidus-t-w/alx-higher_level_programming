51b889eabde1eb92ec37882fa7ac647f0f68bd9e
kidus-t-w <kidusk69@gmail.com>
2024-01-17 16:00:47 +0300

    updated task

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-""" takes in a URL and an email address, sends a POS
-request to the passed URL with the email as a parameter,
-and finally displays the body of the response."""
+""" takes in a URL and an email address, sends a POST request to the passed URL with 
+the email as a parameter, and finally displays the body of the response."""
 import sys
 import requests
 
 url_from_argument = sys.argv[1]
 email_from_argument = sys.argv[2]
 
-response = requests.post(
-    url_from_argument,
+response = requests.post(url_from_argument, 
     data={"email": email_from_argument},
     headers={"Content-Type": "text/plain"},
 )
