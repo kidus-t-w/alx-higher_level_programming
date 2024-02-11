2f7f24ef31aa22206e2df20f28033201af2d4ea4
kidus-t-w <kidusk69@gmail.com>
2024-02-11 22:07:56 +0300

    updated 3-starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,24 @@
 #!/usr/bin/node
-// A script that prints the title of a Star Wars movie
+// Your script description here
 
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+const reqURL = 'https://swapi-api.hbtn.io/api/films/'; // change 'let' to 'const'
+const request = require('request'); // change 'let' to 'const'
+request(reqURL + process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    console.log(JSON.parse(body).title);
+    const jso = JSON.parse(body); // change 'let' to 'const'
+    const results = jso.results; // change 'let' to 'const' and use dot notation
+    let count = 0;
+    for (let i = 0; i < results.length; i++) {
+      const chars = results[i].characters; // change 'let' to 'const' and use dot notation
+      for (let j = 0; j < chars.length; j++) {
+        const check18 = chars[j].includes('18'); // change 'let' to 'const'
+        if (check18) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
 });
